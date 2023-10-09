344e4060e87fae9f402b57908fd20245e2aabea6
Anas Assadek <anasassadek@ceroca.cat>
2023-10-09 17:51:34 +0200

    Prueba Git 2 Cambios

--- Assets/Prueba.cs
+++ Assets/Prueba.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Prueba : MonoBehaviour
+
 {
+    private float velocity = 5f;
     // Start is called before the first frame update
     void Start()
     {

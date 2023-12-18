dbdff261df3ffece86a203c0dc6676daf48c6f29
Anas Assadek <anasassadek@ceroca.cat>
2023-12-18 12:46:35 +0100

    Punto misión

---
+++ Assets/Scripts/Mapa/DesaparecePuntoContacto.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class DestroyOnContact : MonoBehaviour
+{
+    public GameObject personajePrincipal; // Para asignar desde el Inspector
+    public Sprite spriteDown; // Asignar desde el Inspector
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject == personajePrincipal)
+        {
+            // Congelar al personaje (detener su movimiento)
+            Rigidbody2D rb = personajePrincipal.GetComponent<Rigidbody2D>();
+            if (rb != null)
+            {
+                rb.velocity = Vector2.zero;
+                rb.constraints = RigidbodyConstraints2D.FreezeAll; // Congela la posición y la rotación
+            }
+
+            // Cambiar el sprite del personaje principal
+            SpriteRenderer spriteRenderer = personajePrincipal.GetComponent<SpriteRenderer>();
+            if (spriteRenderer != null && spriteDown != null)
+            {
+                spriteRenderer.sprite = spriteDown;
+            }
+
+            // Destruir el objeto PuntoMision
+            Destroy(gameObject);
+        }
+    }
+}
